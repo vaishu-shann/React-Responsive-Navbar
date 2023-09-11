ddb4a20cbb8ea1018277e93ff1d5d21108f12ad7
vaishu-shann <ilukiabi@gmail.com>
2023-09-11 13:55:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
